91888479d273b5c83f053936cbf868c5e07013d5
Gary Zhang <492828+garyzyg@users.noreply.github.com>
2018-08-24 23:53:37 +0800

    enable png support.

--- build.bat
+++ build.bat
@@ -10,8 +10,28 @@ SET Include=%Include%;%CRT_INC_PATH%
 SET Path=%CD%;C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin;%Path%
 
 FOR /F "DELIMS=" %%I IN ('WHERE cmake.exe') DO FOR /F "DELIMS=" %%J IN ('DIR /B /S "%%~dpI..\Windows-MSVC.cmake"') DO FOR %%K IN (
-"/O2 /Ob2		/O1 /GL /GS-"
+"/O2 /Ob2		/O1 /GL /GS- /DPNG_SUPPORTED"
 "\${_RTC1}		 "
 ) DO FOR /F "TOKENS=1,* DELIMS=	" %%L IN (%%K) DO C:\msys64\usr\bin\sed.exe "s@%%L@%%M@" -i "%%J"
 
-cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DWITH_CRT_DLL=1 -DCMAKE_EXE_LINKER_FLAGS=/MANIFEST:NO %CD%& FOR %%I IN (*.1) DO FOR /F "TOKENS=2 DELIMS=( " %%J IN ('FIND "add_executable" CMakeLists.txt ^| FIND "%%~nI"') DO nmake %%J VERBOSE=1
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+FOR /F "DELIMS=" %%I IN ('DIR /O:-N /A:D-H /B') DO (
+	SET Lib=!Lib!;%CD%\%%I
+	SET Include=!Include!;%CD%\%%I
+
+FOR /F %%J IN ('ECHO %%I ^| FIND /I "zlib-"') DO (
+	CD %%J
+	cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS=/MANIFEST:NO& nmake VERBOSE=1 zlibstatic
+	CD ..
+)
+FOR /F %%J IN ('ECHO %%I ^| FIND /I "lpng"') DO FOR /F %%K IN ('DIR /B /A:D zlib-*') DO (
+	CD %%J
+	cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS=/MANIFEST:NO -DZLIB_LIBRARY=${ZLIB_INCLUDE_DIR}\zlibstatic.lib -DZLIB_INCLUDE_DIR=%CD%\..\%%K& nmake VERBOSE=1 png_static
+	CD ..
+)
+
+)
+
+C:\msys64\usr\bin\sed.exe "s@\${CJPEG_BMP_SOURCES}@& rdpng.c@" -i CMakeLists.txt
+cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DWITH_CRT_DLL=1 -DCMAKE_EXE_LINKER_FLAGS="/MANIFEST:NO libpng16_static.lib zlibstatic.lib"& FOR %%I IN (*.1) DO FOR /F "TOKENS=2 DELIMS=( " %%J IN ('FIND "add_executable" CMakeLists.txt ^| FIND "%%~nI"') DO nmake %%J VERBOSE=1
