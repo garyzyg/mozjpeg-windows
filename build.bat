6918002fac83e504144e555eeb64e8595699b2e5
Gary Zhang <492828+garyzyg@users.noreply.github.com>
2019-07-30 08:41:43 +0800

    clean up script and switch compiler to LLVM

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
+SET SED=C:\msys64\usr\bin\sed.exe
+
 PUSHD .
 FOR %%I IN (C:\WinDDK\7600.16385.1) DO CALL %%I\bin\setenv.bat %%I fre %Platform% WIN7 no_oacr
 POPD
@@ -9,10 +11,23 @@ SET Include=%Include%;%CRT_INC_PATH%
 
 SET Path=%CD%;C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin;%Path%
 
+FOR /F "DELIMS=" %%I IN ('WHERE cl.exe') DO %SED% "/_Complex/d" -i "%%~dpI..\..\..\inc\crt\math.h"
+
+FOR %%I IN (
+clang-cl.exe
+lld-link.exe
+llvm-lib.exe
+) DO ^
+FOR /F "TOKENS=2 DELIMS=-" %%J IN ('ECHO %%I') DO ^
+FOR /F "DELIMS=" %%K IN ('WHERE %%I') DO ^
+SET Path=%%~dpK;%Path%& MKLINK /H "%%~dpK%%J" "%%K"
+
+IF %Platform%==x64 (SET M64=-m64) ELSE SET M64=-m32
+
 FOR /F "DELIMS=" %%I IN ('WHERE cmake.exe') DO FOR /F "DELIMS=" %%J IN ('DIR /B /S "%%~dpI..\Windows-MSVC.cmake"') DO FOR %%K IN (
-"/O2 /Ob2		/O1 /GL /GS- /DPNG_SUPPORTED"
-"\${_RTC1}		 "
-) DO FOR /F "TOKENS=1,* DELIMS=	" %%L IN (%%K) DO C:\msys64\usr\bin\sed.exe "s@%%L@%%M@" -i "%%J"
+"/O2 /Ob2		/O1 -flto /GS- /DPNG_SUPPORTED %M64%"
+"\${_RTC1}		%M64%"
+) DO FOR /F "TOKENS=1,* DELIMS=	" %%L IN (%%K) DO %SED% "s@%%L@%%M@" -i "%%J"
 
 SETLOCAL ENABLEDELAYEDEXPANSION
 
@@ -22,16 +37,20 @@ FOR /F "DELIMS=" %%I IN ('DIR /O:-N /A:D-H /B') DO (
 
 FOR /F %%J IN ('ECHO %%I ^| FIND /I "zlib-"') DO (
 	CD %%J
-	cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS=/MANIFEST:NO& nmake VERBOSE=1 zlibstatic
+	cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS=%M64%& nmake VERBOSE=1 zlibstatic
 	CD ..
+	SET ZLIB_INCLUDE_DIR=%CD%\%%J
+	SET ZLIB_LIBRARY=!ZLIB_INCLUDE_DIR!\zlibstatic.lib
 )
-FOR /F %%J IN ('ECHO %%I ^| FIND /I "lpng"') DO FOR /F %%K IN ('DIR /B /A:D zlib-*') DO (
+FOR /F %%J IN ('ECHO %%I ^| FIND /I "lpng"') DO (
 	CD %%J
-	cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS=/MANIFEST:NO -DZLIB_LIBRARY=${ZLIB_INCLUDE_DIR}\zlibstatic.lib -DZLIB_INCLUDE_DIR=%CD%\..\%%K& nmake VERBOSE=1 png_static
+	cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS=%M64% -DZLIB_INCLUDE_DIR=!ZLIB_INCLUDE_DIR! -DZLIB_LIBRARY=!ZLIB_LIBRARY!& nmake VERBOSE=1 png_static
 	CD ..
+	SET PNG_PNG_INCLUDE_DIR=%CD%\%%J
+	SET PNG_LIBRARY=!PNG_PNG_INCLUDE_DIR!\libpng16_static.lib
 )
 
 )
 
-C:\msys64\usr\bin\sed.exe "s@\${CJPEG_BMP_SOURCES}@& rdpng.c@" -i CMakeLists.txt
-cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DWITH_CRT_DLL=1 -DCMAKE_EXE_LINKER_FLAGS="/MANIFEST:NO libpng16_static.lib zlibstatic.lib"& FOR %%I IN (*.1) DO FOR /F "TOKENS=2 DELIMS=( " %%J IN ('FIND "add_executable" CMakeLists.txt ^| FIND "%%~nI"') DO nmake %%J VERBOSE=1
+%SED% "s@\${CJPEG_BMP_SOURCES}@& rdpng.c@" -i CMakeLists.txt
+cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="%M64% -fno-builtin-pow" -DZLIB_LIBRARY=%ZLIB_LIBRARY% -DPNG_LIBRARY=%PNG_LIBRARY% -DPNG_PNG_INCLUDE_DIR=%PNG_PNG_INCLUDE_DIR% -DWITH_CRT_DLL=1 -DCMAKE_EXE_LINKER_FLAGS="/MANIFEST:NO libpng16_static.lib zlibstatic.lib"& FOR %%I IN (*.1) DO FOR /F "TOKENS=2 DELIMS=( " %%J IN ('FIND "add_executable" CMakeLists.txt ^| FIND "%%~nI"') DO nmake %%J VERBOSE=1
